9de3e07ec27e7651029e47cd50731827549eec14
horoyoii <demonic3540@naver.com>
2020-04-02 13:43:13 +0900

    Build with gradle

---
+++ src/main/java/org/horoyoii/loadbalance/LoadBalancer.java
@@ -0,0 +1,20 @@
+package org.horoyoii.loadbalance;
+
+import java.io.*;
+
+public class LoadBalancer{
+    
+    
+
+    public void readConfig(){
+        System.out.println("read conf");
+        //File f = new File(path);
+        //FileReader fr = new FileReader();
+    }
+
+    public void run(){
+        System.out.println("Runnin...");
+    }
+
+}
+
