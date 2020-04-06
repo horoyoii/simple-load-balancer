b96fe74ae4dac099ade55680af76497c1eb16cdb
horoyoii <demonic3540@naver.com>
2020-04-07 00:30:37 +0900

    Add shell scripting for multiple test clients

---
+++ clients/Client.java
@@ -0,0 +1,50 @@
+import java.net.*;
+import java.io.*;
+
+class Client{
+    private static int id;
+
+    public static void main(String[] args){
+        
+        if(args.length != 2){
+            log("no args");
+            System.exit(1);   
+        }
+        
+        int port = Integer.parseInt(args[0]);
+        id = Integer.parseInt(args[1]);
+        
+
+        try{
+            Socket socket = new Socket("127.0.0.1", port);
+            
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+
+            String data = "ABCD";
+
+            out.write(data.getBytes());
+            
+            log("send done");
+            
+
+            byte[] recData = new byte[10];
+
+            in.read(recData);
+            
+            log("read done");
+
+            String rec = new String(recData);
+
+            log("result : "+rec);
+            
+        }catch(Exception e){
+            log(e.toString());
+        }
+    }
+
+    public static void log(String msg){
+        System.out.println("[INFO - "+id+" ] : "+msg);
+    }
+
+}
---
+++ clients/run.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+
+echo "---------------------"
+echo "| how many client ? |"
+echo "---------------------"
+echo -n "port num : "
+read port
+
+echo -n "number : "
+
+read num
+
+for i in `seq $num`
+do
+    java Client $port $i
+done
